b8b0c5f554169f3eca45f3726d3e9313c1a6fb13
vmcooper628 <130080349+vmcooper628@users.noreply.github.com>
2023-04-26 19:12:53 -0700

    adding lab 5

---
+++ Desktop/art101/lab5/js/lab.js
@@ -0,0 +1,20 @@
+/*
+* Author: Anthony Cooper <vmcooper@ucsc.edu>
+* Created 26 April
+* License: Publc domain
+*/
+
+make = "Ford";
+model = "Fusion";
+color = "Silver";
+year = 2018;
+
+age = 2023 - year
+
+ownIt = false;
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
